ba6c272d217a7ee4feeb005b44f238a3562dfdfc
Jerel Miller <jerelmiller@gmail.com>
2026-01-28 12:50:33 -0700

    Add options to dummy AC instance

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import type { ApolloClient as BaseApolloClient } from "@apollo/client";
+import type { ApplicationManifest } from "./types/application-manifest.js";
+
 export type {
   ApplicationManifest,
   ManifestOperation,
@@ -8,8 +11,15 @@ export type {
   ManifestWidgetSettings,
 } from "./types/application-manifest.js";
 
+export declare namespace ApolloClient {
+  export interface Options extends Omit<BaseApolloClient.Options, "link"> {
+    link?: BaseApolloClient.Options["link"];
+    manifest: ApplicationManifest;
+  }
+}
+
 export class ApolloClient {
-  constructor() {
+  constructor(options: ApolloClient.Options) {
     throw new Error(
       "Cannot construct an `ApolloClient` instance from `@apollo/client-ai-apps` without export conditions. Please set conditions or import from the `/openai` or `/mcp` subpath directly."
     );
