d24f7f64f85583388043ae2c835c71fbe8343a3a
Jerel Miller <jerelmiller@gmail.com>
2026-02-06 12:07:26 -0700

    Change defaults to use runtime value as type

--- src/testing/internal/openai/stubOpenAiGlobals.ts
+++ src/testing/internal/openai/stubOpenAiGlobals.ts
@@ -9,23 +9,23 @@ import { dispatchStateChange } from "./dispatchStateChange.js";
 
 type Globals = Partial<API<any> & OpenAiGlobals>;
 
-export function stubOpenAiGlobals(
-  globals?: Globals | ((defaults: Partial<Globals>) => Partial<Globals>)
-) {
-  const defaults = {
-    setWidgetState: async (state: UnknownObject) => {
-      window.openai.widgetState = state;
-      dispatchStateChange();
-    },
-    // Using a `null` here instead of `undefined` allows for the client to fully
-    // initialize without having to wait for the global openAI event.
-    toolOutput: null,
-  } satisfies Partial<Globals>;
+const DEFAULTS = Object.freeze({
+  setWidgetState: async (state: UnknownObject) => {
+    window.openai.widgetState = state;
+    dispatchStateChange();
+  },
+  // Using a `null` here instead of `undefined` allows for the client to fully
+  // initialize without having to wait for the global openAI event.
+  toolOutput: null,
+}) satisfies Partial<Globals>;
 
+export function stubOpenAiGlobals(
+  globals?: Globals | ((defaults: typeof DEFAULTS) => Partial<Globals>)
+) {
   vi.stubGlobal(
     "openai",
     typeof globals === "function" ?
-      globals(defaults)
-    : { ...defaults, ...globals }
+      globals(DEFAULTS)
+    : { ...DEFAULTS, ...globals }
   );
 }
