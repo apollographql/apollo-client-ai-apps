31d0577e0bb98701b213cbbd73a9e6bea7d74e51
Jerel Miller <jerelmiller@gmail.com>
2026-02-10 16:47:12 -0700

    Add check for client type in useApolloClient

--- src/mcp/react/hooks/useApolloClient.ts
+++ src/mcp/react/hooks/useApolloClient.ts
@@ -1,6 +1,15 @@
 import { useApolloClient as useBaseApolloClient } from "@apollo/client/react";
 import { ApolloClient } from "../../core/ApolloClient";
+import { invariant } from "@apollo/client/utilities/invariant";
+import { aiClientSymbol } from "../../../utilities";
 
-export const useApolloClient = useBaseApolloClient as (
-  override?: ApolloClient
-) => ApolloClient;
+export function useApolloClient(override?: ApolloClient) {
+  const client = useBaseApolloClient(override) as ApolloClient;
+
+  invariant(
+    client[aiClientSymbol],
+    'The "client" instance provided to <ApolloProvider /> is the wrong instance. You might have imported `ApolloClient` from `@apollo/client`. Please import `ApolloClient` from `@apollo/client-ai-apps` instead.'
+  );
+
+  return client;
+}
