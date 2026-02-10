46c63378f61be450deb10f47618cd08b3ff0126a
Jerel Miller <jerelmiller@gmail.com>
2026-02-10 16:12:27 -0700

    Add back doc block

--- src/link/ToolCallLink.ts
+++ src/link/ToolCallLink.ts
@@ -1,5 +1,27 @@
 import { ApolloLink } from "@apollo/client";
 
+/**
+ * A terminating link that sends a GraphQL request through an agent tool call.
+ * When providing a custom link chain to `ApolloClient`, `ApolloClient` will
+ * validate that the terminating link is an instance of this link.
+ *
+ * @example Provding a custom link chain
+ *
+ * ```ts
+ * import { ApolloLink } from "@apollo/client";
+ * import { ApolloClient, ToolCallLink } from "@apollo/client-ai-apps";
+ *
+ * const link = ApolloLink.from([
+ *   ...otherLinks,
+ *   new ToolCallLink()
+ * ]);
+ *
+ * const client = new ApolloClient({
+ *   link,
+ *   // ...
+ * });
+ * ```
+ */
 export class ToolCallLink extends ApolloLink {
   constructor() {
     super();
