6d2dfd3828cc2db7a34fb7c41355bd40c13179e5
Jerel Miller <jerelmiller@gmail.com>
2026-02-10 18:21:04 -0700

    Don't export base plugin

--- src/vite/index.ts
+++ src/vite/index.ts
@@ -1,7 +1,3 @@
 export * from "./application_manifest_plugin.js";
 export * from "./absolute_asset_imports_plugin.js";
-export {
-  ApolloClientAiAppsPlugin,
-  BaseApolloClientAiAppsPlugin,
-  devTarget,
-} from "./apolloClientAiApps.js";
+export { ApolloClientAiAppsPlugin, devTarget } from "./apolloClientAiApps.js";
