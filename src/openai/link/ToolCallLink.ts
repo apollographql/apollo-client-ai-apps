cfd83cf381ef14a3694f2bba206c06de69ce30fa
Jerel Miller <jerelmiller@gmail.com>
2026-02-10 18:15:36 -0700

    Remove selectHttpOptionsAndBody call in ToolCallLink

--- src/openai/link/ToolCallLink.ts
+++ src/openai/link/ToolCallLink.ts
@@ -1,9 +1,6 @@
 import { ApolloLink, Observable } from "@apollo/client";
+import { print } from "@apollo/client/utilities";
 import { from, map } from "rxjs";
-import {
-  fallbackHttpConfig,
-  selectHttpOptionsAndBody,
-} from "@apollo/client/link/http";
 
 /**
  * A terminating link that sends a GraphQL request through an agent tool call.
@@ -29,21 +26,10 @@ import {
  */
 export class ToolCallLink extends ApolloLink {
   request(operation: ApolloLink.Operation): Observable<ApolloLink.Result> {
-    const context = operation.getContext();
-    const contextConfig = {
-      http: context.http,
-      options: context.fetchOptions,
-      credentials: context.credentials,
-      headers: context.headers,
-    };
-    const { query, variables } = selectHttpOptionsAndBody(
-      operation,
-      fallbackHttpConfig,
-      contextConfig
-    ).body;
+    const { query, variables } = operation;
 
-    return from(window.openai.callTool("execute", { query, variables })).pipe(
-      map((result) => ({ data: result.structuredContent.data }))
-    );
+    return from(
+      window.openai.callTool("execute", { query: print(query), variables })
+    ).pipe(map((result) => ({ data: result.structuredContent.data })));
   }
 }
