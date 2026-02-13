bf44a41677ad8ddb6b5bd0831dbae5f8ee4f08d7
Jerel Miller <jerelmiller@gmail.com>
2026-02-13 01:00:20 -0700

    Encode the labels format in the zod schema

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -6,6 +6,24 @@ const Mode = z
   .literal(["development", "production"])
   .or(z.custom<string & {}>((value) => typeof value === "string"));
 
+const labelsInput = z.exactOptional(
+  z.strictObject({
+    toolInvocation: z.exactOptional(
+      z.strictObject({
+        invoking: z.string().exactOptional(),
+        invoked: z.string().exactOptional(),
+      })
+    ),
+  })
+);
+
+const labelsOutput = z.exactOptional(
+  z.strictObject({
+    "toolInvocation/invoking": z.string().exactOptional(),
+    "toolInvocation/invoked": z.string().exactOptional(),
+  })
+);
+
 export const ApolloClientAiAppsConfigSchema = z.strictObject({
   name: z.string().exactOptional(),
   description: z.string().exactOptional(),
@@ -31,14 +49,25 @@ export const ApolloClientAiAppsConfigSchema = z.strictObject({
       domain: z.string().exactOptional(),
     })
   ),
-  labels: z.exactOptional(
-    z.strictObject({
-      toolInvocation: z.exactOptional(
-        z.strictObject({
-          invoking: z.string().exactOptional(),
-          invoked: z.string().exactOptional(),
-        })
-      ),
-    })
-  ),
+  labels: z.codec(labelsInput, labelsOutput, {
+    encode: () => {
+      // encode is currently not used so we can skip the business logic and
+      // return an empty object which satisfies the type. If we need to encode
+      // labels back to the original form, this will need to be implemented
+      return {};
+    },
+    decode: ({ toolInvocation }) => {
+      const config: z.infer<typeof labelsOutput> = {};
+
+      if (toolInvocation?.invoking) {
+        config["toolInvocation/invoking"] = toolInvocation.invoking;
+      }
+
+      if (toolInvocation?.invoked) {
+        config["toolInvocation/invoked"] = toolInvocation.invoked;
+      }
+
+      return config;
+    },
+  }),
 });
