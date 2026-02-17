f2c5805ab093abd3d61cfb9222a82c759a01ad86
Jerel Miller <jerelmiller@gmail.com>
2026-02-17 13:38:50 -0700

    Add index signature

--- src/core/types.ts
+++ src/core/types.ts
@@ -5,6 +5,7 @@ export namespace ApolloMcpServerApps {
   export interface StructuredContent {
     result: FormattedExecutionResult;
     prefetch?: Record<string, FormattedExecutionResult>;
+    [x: string]: unknown;
   }
 
   export interface CallToolResult extends Omit<
