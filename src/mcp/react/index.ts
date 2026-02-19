5ecb96d96b6efc781445058cc0212e643128cc5d
Jerel Miller <jerelmiller@gmail.com>
2026-02-18 18:09:04 -0700

    Move /mcp and /openai react code to /react/index.js

---
+++ src/mcp/react/index.ts
@@ -0,0 +1,4 @@
+export { useApp } from "./hooks/useApp.js";
+export { useToolName } from "./hooks/useToolName.js";
+export { useToolMetadata } from "./hooks/useToolMetadata.js";
+export { useToolInput } from "./hooks/useToolInput.js";
