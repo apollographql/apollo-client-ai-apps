913cbd002770c545a4489a4a6a9b7a0eeebcb569
Jerel Miller <jerelmiller@gmail.com>
2026-02-18 18:15:23 -0700

    Simplify types

--- src/react/index.ts
+++ src/react/index.ts
@@ -5,19 +5,15 @@ export { ApolloProvider } from "./ApolloProvider.js";
 // Use `mcp` related types since these are the most common between the two
 // targets
 export const useApp =
-  missingHook<typeof import("../mcp/react/hooks/useApp.js").useApp>("useApp");
+  missingHook<typeof import("./index.mcp.js").useApp>("useApp");
 
 export const useToolInput =
-  missingHook<typeof import("../mcp/react/hooks/useToolInput.js").useToolInput>(
-    "useToolInput"
-  );
+  missingHook<typeof import("./index.mcp.js").useToolInput>("useToolInput");
 
 export const useToolMetadata =
-  missingHook<
-    typeof import("../mcp/react/hooks/useToolMetadata.js").useToolMetadata
-  >("useToolMetadata");
+  missingHook<typeof import("./index.mcp.js").useToolMetadata>(
+    "useToolMetadata"
+  );
 
 export const useToolName =
-  missingHook<typeof import("../mcp/react/hooks/useToolName.js").useToolName>(
-    "useToolName"
-  );
+  missingHook<typeof import("./index.mcp.js").useToolName>("useToolName");
