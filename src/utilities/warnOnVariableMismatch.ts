6e1d2f12d12680fcb0dd7282dd6cc04551306f39
Jerel Miller <jerelmiller@gmail.com>
2026-02-25 17:52:08 -0700

    Tweak warning

--- src/utilities/warnOnVariableMismatch.ts
+++ src/utilities/warnOnVariableMismatch.ts
@@ -9,8 +9,9 @@ export function warnOnVariableMismatch(
   const operationName = getOperationName(document, "(anonymous)");
 
   console.warn(
-    `The operation "${operationName}" has a @tool directive matching the current tool call, but the ` +
-      "variables don't match the tool input. Use the `useHydratedVariables` hook returned from `createHydrationUtils` " +
+    `The operation "${operationName}" has a @tool directive matching the current ` +
+      "tool call, but the variables provided to the query don't match the tool " +
+      "input. Use the `useHydratedVariables` hook returned from `createHydrationUtils` " +
       "to provide the hydrated variables to the query. " +
       "\n\nExpected variables:\n%o\n\nReceived:\n%o",
     received,
