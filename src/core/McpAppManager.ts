b456a18c44124f35f1040c3784780a94364e3ede
Jerel Miller <jerelmiller@gmail.com>
2026-03-24 17:31:22 -0600

    Add generic McpAppManager

---
+++ src/core/McpAppManager.ts
@@ -0,0 +1,106 @@
+import {
+  App,
+  type McpUiHostContextChangedNotification,
+} from "@modelcontextprotocol/ext-apps";
+import type { CallToolResult } from "@modelcontextprotocol/sdk/types.js";
+import type { ApplicationManifest } from "../types/application-manifest.js";
+import type { FormattedExecutionResult } from "graphql";
+import type { DocumentNode, OperationVariables } from "@apollo/client";
+import { print } from "@apollo/client/utilities";
+import { cacheAsync } from "../utilities/index.js";
+import type { ApolloMcpServerApps } from "./types.js";
+
+type ExecuteQueryCallToolResult = Omit<CallToolResult, "structuredContent"> & {
+  structuredContent: FormattedExecutionResult;
+  _meta?: { structuredContent?: FormattedExecutionResult };
+};
+
+interface ConnectToHostResult {
+  structuredContent: ApolloMcpServerApps.StructuredContent;
+  _meta: ApolloMcpServerApps.Meta | undefined;
+  toolInput: Record<string, unknown> | undefined;
+  toolName: string | undefined;
+}
+
+type ConnectToHostImplementation = (app: App) => Promise<ConnectToHostResult>;
+
+/** @internal */
+export class McpAppManager {
+  readonly app: App;
+
+  #toolName: string | undefined;
+  #toolMetadata: ApolloMcpServerApps.CallToolResult["_meta"] | undefined;
+  #toolInput: Record<string, unknown> | undefined;
+
+  #connectToHost: ConnectToHostImplementation;
+
+  #hostContextCallbacks = new Set<
+    (params: McpUiHostContextChangedNotification["params"]) => void
+  >();
+
+  constructor(
+    manifest: ApplicationManifest,
+    connectToHost: ConnectToHostImplementation
+  ) {
+    this.app = new App({ name: manifest.name, version: manifest.appVersion });
+    this.#connectToHost = connectToHost;
+  }
+
+  get toolName() {
+    return this.#toolName;
+  }
+
+  get toolMetadata() {
+    return this.#toolMetadata;
+  }
+
+  get toolInput() {
+    return this.#toolInput;
+  }
+
+  onHostContextChanged(
+    cb: (params: McpUiHostContextChangedNotification["params"]) => void
+  ) {
+    this.#hostContextCallbacks.add(cb);
+
+    return () => {
+      this.#hostContextCallbacks.delete(cb);
+    };
+  }
+
+  connect = cacheAsync(async () => {
+    this.app.onhostcontextchanged = (params) => {
+      this.#hostContextCallbacks.forEach((cb) => cb(params));
+    };
+
+    const result = await this.#connectToHost(this.app);
+
+    this.#toolName = result.toolName;
+    this.#toolInput = result.toolInput;
+    this.#toolMetadata = result._meta;
+
+    return result;
+  });
+
+  close() {
+    return this.app.close();
+  }
+
+  async executeQuery({
+    query,
+    variables,
+  }: {
+    query: DocumentNode;
+    variables: OperationVariables | undefined;
+  }) {
+    const result = (await this.app.callServerTool({
+      name: "execute",
+      arguments: { query: print(query), variables },
+    })) as ExecuteQueryCallToolResult;
+
+    return {
+      ...result.structuredContent,
+      ...result._meta?.structuredContent,
+    };
+  }
+}
