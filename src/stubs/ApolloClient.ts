4a1852472f1329703499e91ab2bb61746fd3be51
Jerel Miller <jerelmiller@gmail.com>
2026-03-24 18:01:34 -0600

    Move stubbed client to stubs/ApolloClient

---
+++ src/stubs/ApolloClient.ts
@@ -0,0 +1,25 @@
+import { ApolloClient as BaseApolloClient } from "@apollo/client";
+import type { ApplicationManifest } from "../types/application-manifest.js";
+import { aiClientSymbol } from "../utilities/constants.js";
+
+export declare namespace ApolloClient {
+  export interface Options extends Omit<BaseApolloClient.Options, "link"> {
+    link?: BaseApolloClient.Options["link"];
+    manifest: ApplicationManifest;
+  }
+}
+
+export class ApolloClient extends BaseApolloClient {
+  /** @internal */
+  readonly [aiClientSymbol] = true;
+
+  constructor(options: ApolloClient.Options) {
+    super(options as any);
+
+    throw new Error(
+      "Cannot construct an `ApolloClient` instance from `@apollo/client-ai-apps` without export conditions. Please set conditions or import from the `/openai` or `/mcp` subpath directly."
+    );
+  }
+
+  async connect() {}
+}
