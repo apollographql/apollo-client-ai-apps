21489b0e4cca8b07c7383de0c7957dd9feb104ac
Jerel Miller <jerelmiller@gmail.com>
2026-03-25 14:50:42 -0600

    Test ToolCallLink in each host env

--- src/link/__tests__/ToolCallLink.test.ts
+++ src/link/__tests__/ToolCallLink.test.ts
@@ -1,112 +1,92 @@
 import { expect, test } from "vitest";
 import { gql, InMemoryCache } from "@apollo/client";
 import { execute } from "@apollo/client/link";
-import { ApolloClient } from "../../mcp/core/ApolloClient.js";
 import {
+  eachHostEnv,
   minimalHostContextWithToolName,
   mockApplicationManifest,
-  mockMcpHost,
   ObservableStream,
   spyOnConsole,
 } from "../../testing/internal/index.js";
 import { ToolCallLink } from "../ToolCallLink.js";
 
-test("merges _meta.structuredContent into result for @private fields", async () => {
-  using _ = spyOnConsole("debug");
-  const query = gql`
-    query GreetingQuery {
-      greeting @private
-    }
-  `;
+eachHostEnv((setupHost, ApolloClient) => {
+  test("merges _meta.structuredContent into result for @private fields", async () => {
+    using _ = spyOnConsole("debug");
+    const query = gql`
+      query GreetingQuery {
+        greeting @private
+      }
+    `;
 
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    manifest: mockApplicationManifest(),
-  });
+    const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      manifest: mockApplicationManifest(),
+    });
 
-  using host = await mockMcpHost({
-    hostContext: minimalHostContextWithToolName("GetProduct"),
-  });
-  host.onCleanup(() => client.stop());
+    const { host } = await setupHost({
+      client,
+      autoTriggerTool: true,
+      hostContext: minimalHostContextWithToolName("GetProduct"),
+      structuredContent: { result: { data: { product: null } } },
+    });
+    using _host = host;
 
-  host.sendToolInput({ arguments: {} });
-  host.sendToolResult({
-    _meta: { toolName: "GetProduct" },
-    content: [],
-    structuredContent: {
-      result: {
-        data: {
-          product: null,
-        },
+    host.mockToolCall("execute", () => ({
+      structuredContent: {},
+      _meta: {
+        structuredContent: { data: { greeting: "Hello, private world" } },
       },
-    },
+    }));
+
+    await client.connect();
+
+    const observable = execute(new ToolCallLink(), { query }, { client });
+    const stream = new ObservableStream(observable);
+
+    await expect(stream).toEmitValue({
+      data: { greeting: "Hello, private world" },
+    });
+
+    await expect(stream).toComplete();
   });
 
-  host.mockToolCall("execute", () => ({
-    structuredContent: {},
-    _meta: {
-      structuredContent: { data: { greeting: "Hello, private world" } },
-    },
-  }));
+  test("delegates query execution to MCP host", async () => {
+    using _ = spyOnConsole("debug");
+    const query = gql`
+      query GreetingQuery {
+        greeting
+      }
+    `;
 
-  await client.connect();
+    const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      manifest: mockApplicationManifest(),
+    });
 
-  const observable = execute(new ToolCallLink(), { query }, { client });
-  const stream = new ObservableStream(observable);
+    const { host } = await setupHost({
+      client,
+      autoTriggerTool: true,
+      hostContext: minimalHostContextWithToolName("GetProduct"),
+      structuredContent: { result: { data: { product: null } } },
+    });
+    using _host = host;
 
-  await expect(stream).toEmitValue({
-    data: { greeting: "Hello, private world" },
-  });
-
-  await expect(stream).toComplete();
-});
-
-test("delegates query execution to MCP host", async () => {
-  using _ = spyOnConsole("debug");
-  const query = gql`
-    query GreetingQuery {
-      greeting
-    }
-  `;
-
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    manifest: mockApplicationManifest(),
-  });
-
-  using host = await mockMcpHost({
-    hostContext: minimalHostContextWithToolName("GetProduct"),
-  });
-  host.onCleanup(() => client.stop());
-
-  host.sendToolInput({ arguments: {} });
-  host.sendToolResult({
-    _meta: { toolName: "GetProduct" },
-    content: [],
-    structuredContent: {
-      result: {
-        data: {
-          product: null,
-        },
+    host.mockToolCall("execute", () => ({
+      structuredContent: {
+        data: { greeting: "Hello, world" },
       },
-    },
-  });
+    }));
 
-  host.mockToolCall("execute", () => ({
-    content: [],
-    structuredContent: {
+    await client.connect();
+
+    const observable = execute(new ToolCallLink(), { query }, { client });
+    const stream = new ObservableStream(observable);
+
+    await expect(stream).toEmitValue({
       data: { greeting: "Hello, world" },
-    },
-  }));
+    });
 
-  await client.connect();
-
-  const observable = execute(new ToolCallLink(), { query }, { client });
-  const stream = new ObservableStream(observable);
-
-  await expect(stream).toEmitValue({
-    data: { greeting: "Hello, world" },
+    await expect(stream).toComplete();
   });
-
-  await expect(stream).toComplete();
 });
