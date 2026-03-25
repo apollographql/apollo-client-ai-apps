b919a91b9c5842dffd1a483f3c6b230b45639335
Jerel Miller <jerelmiller@gmail.com>
2026-03-25 11:00:57 -0600

    Remove individual createHydrationUtils implementations

--- src/mcp/react/index.ts
+++
@@ -1,1 +0,0 @@
-export { createHydrationUtils } from "./hooks/createHydrationUtils.js";
--- src/openai/react/index.ts
+++ src/openai/react/index.ts
@@ -1,2 +1,1 @@
-export { createHydrationUtils } from "./hooks/createHydrationUtils.js";
 export { useWidgetState } from "./hooks/useWidgetState.js";
