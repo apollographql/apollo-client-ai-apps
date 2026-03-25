3375585f012d6bb77b68bacb06e4c316eeba93c9
Jerel Miller <jerelmiller@gmail.com>
2026-03-25 10:45:18 -0600

    Add shared createHydrationUtils

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,5 +1,3 @@
-import { missingHook } from "./missingHook.js";
-
 export { ApolloProvider } from "./ApolloProvider.js";
 export { reactive } from "./reactive.js";
 export type { Reactive } from "./reactive.js";
@@ -8,11 +6,4 @@ export { useApp } from "./hooks/useApp.js";
 export { useHostContext } from "./hooks/useHostContext.js";
 export { useToolInfo } from "./hooks/useToolInfo.js";
 export { useToolMetadata } from "./hooks/useToolMetadata.js";
-
-// Use `mcp` related types since these are the most common between the two
-// targets
-
-/** @experimental */
-export const createHydrationUtils = missingHook<
-  typeof import("./index.mcp.js").createHydrationUtils
->("createHydrationUtils");
+export { createHydrationUtils } from "./createHydrationUtils.js";
