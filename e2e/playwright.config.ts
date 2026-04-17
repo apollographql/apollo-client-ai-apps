e4b8b016af52bdcf6d785618e127ae9d208ac1ae
Jerel Miller <jerelmiller@gmail.com>
2026-04-17 12:35:29 -0600

    Move some files around

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: "./tests",
   globalTeardown: "./e2e/global-teardown.js",
   webServer: [
     {
