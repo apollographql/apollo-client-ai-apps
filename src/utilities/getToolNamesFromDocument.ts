5812fcd8565008aa55f430432cb74b702222d877
Jerel Miller <jerelmiller@gmail.com>
2026-05-13 00:08:35 -0600

    Fix tool match when `@tool` doesn't use arguments

--- src/utilities/getToolNamesFromDocument.ts
+++ src/utilities/getToolNamesFromDocument.ts
@@ -1,15 +1,27 @@
 import { getOperationDefinition } from "@apollo/client/utilities/internal";
-import { Kind, type DocumentNode } from "graphql";
+import { Kind, type DirectiveNode, type DocumentNode } from "graphql";
 
 export function getToolNamesFromDocument(document: DocumentNode) {
   const operationDef = getOperationDefinition(document);
+  const operationName = operationDef?.name?.value;
+  const toolDirectives =
+    operationDef?.directives?.filter((d) => d.name.value === "tool") ?? [];
+
+  if (toolDirectives.length === 1) {
+    return new Set([
+      getToolNameFromDirective(toolDirectives[0]) ?? operationName,
+    ]);
+  }
 
   return new Set(
-    operationDef?.directives
-      ?.filter((d) => d.name.value === "tool")
-      .flatMap((d) => {
-        const nameArg = d.arguments?.find((arg) => arg.name.value === "name");
-        return nameArg?.value.kind === Kind.STRING ? [nameArg.value.value] : [];
-      })
+    toolDirectives.flatMap((d) => {
+      const name = getToolNameFromDirective(d);
+      return name ? [name] : [];
+    })
   );
+}
+
+function getToolNameFromDirective(directive: DirectiveNode) {
+  const nameArg = directive.arguments?.find((arg) => arg.name.value === "name");
+  return nameArg?.value.kind === Kind.STRING ? nameArg.value.value : undefined;
 }
